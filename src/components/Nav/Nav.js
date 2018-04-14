e30632c42c5d2571aa6b4c528fa809ba77a61c63
Jmactiag <jessie.macatiag@gmail.com>
2018-04-14 13:49:38 -0700

    add additional styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,22 +3,13 @@ import { Navbar } from 'react-bootstrap';
 import "./nav.css";
 
 const Nav = props => (
-    // <nav className='navbar justify-content-center'>
-    //     <ul>
-    //         <li className='brand nav-link'>
-    //             <h1>Clicky Game</h1>
-    //         </li>
-    //         <li className='nav-link'>
-    //             {props.children}
-    //         </li>
-    //     </ul>
-    // </nav>
+
 
     <Navbar>
-        <Navbar.Text className="name col-md-8">
+        <Navbar.Text className="name test">
             Clicky Game
         </Navbar.Text>
-        <Navbar.Text className="scores col-md-4">
+        <Navbar.Text className="scores test">
             {props.children}
         </Navbar.Text>
     </Navbar>
